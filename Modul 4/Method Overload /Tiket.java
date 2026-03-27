a28db4fe3ff6e02f9a804df1ce35aba11948acb7
piyowna <vpmaghfiroh@student.ub.ac.id>
2026-03-27 17:16:21 +0700

    Create Tiket.java

---
+++ Modul 4/Method Overload /Tiket.java
@@ -0,0 +1,35 @@
+public class Tiket { 
+ protected String namaPenumpang = "Anonim"; 
+ protected String tujuan = "Jakarta"; 
+ protected int jarakTempuh = 750; 
+ protected double hargaTiket = 250000; 
+
+    public void pesan() {} 
+        public void pesan(String n, String t) { 
+            this.namaPenumpang = n; 
+            this.tujuan = t;
+            this.jarakTempuh = 750; 
+            this.hargaTiket = 250000; 
+    }
+
+    public void pesan(String n, String t, int j, double h) { 
+        this.namaPenumpang = n; 
+        this.tujuan = t; 
+        this.jarakTempuh = j; 
+        this.hargaTiket = h; 
+    }
+
+    public void tampil() { 
+        System.out.println("Nama Penumpang: " + this.namaPenumpang); 
+        System.out.println("Tujuan: " + this.tujuan); 
+        System.out.println("Jarak Tempuh: " + this.jarakTempuh); 
+        System.out.println("Harga Tiket: " + this.hargaTiket); 
+        System.out.println("--------------------"); 
+    }
+
+    public void pesan(String n, int j) { 
+        this.namaPenumpang = n;
+        this.jarakTempuh = j;
+    } 
+
+}
